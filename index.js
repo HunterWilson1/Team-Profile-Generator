97d8f9aaabf377a11b82f90f240128ec599c559b
Hunter Wilson <huntertheguy123@gmail.com>
2023-03-06 17:07:11 -0600

    I added the functions at the bottom of index.js so that when I fill in the inquirer prompt it will take the data from that and then create a html

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const inquirer = require('inquirer')
-const fs = require('fs')
+const fs = require('fs');
+const markdownHTML = require('./src/HTML');
 
+function makeTeam(){
+
+}
 const manager = [
     {
         type: "input",
@@ -8,3 +12,22 @@ const manager = [
         name: "name"
     }
 ]
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Everything is good!")
+        }
+    });
+}
+
+function writeHTML() {
+    inquirer.prompt().then((data) => {
+        const HTML = markdownHTML(data);
+        writeToFile('index.html', HTML)
+    })
+}
+
+writeHTML();
