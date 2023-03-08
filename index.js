991d618e729ea747b5a1c88a6ea928e8e2feb991
Hunter Wilson <huntertheguy123@gmail.com>
2023-03-07 20:00:35 -0600

    I finished the prompts for inquirer and then got rid of the previous functions that would write the html

--- index.js
+++ index.js
@@ -1,33 +1,82 @@
-const inquirer = require('inquirer')
-const fs = require('fs');
-const markdownHTML = require('./src/HTML');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const markdownHTML = require("./src/HTML");
+const classes = require("./lib")
 
-function makeTeam(){
+function makeTeam() {
+  const team = [];
 
-}
-const manager = [
+  const manager = [
     {
-        type: "input",
-        message: "Please enter manager's name",
-        name: "name"
-    }
-]
+      type: "input",
+      message: "Please enter manager's name: ",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "Please enter Manager ID: ",
+      name: "id",
+    },
+    {
+      type: "input",
+      message: "Please enter Manager email address: ",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "Please enter the office number: ",
+      name: "officeNumber",
+    },
+  ];
+  const engineer = [
+    {
+      type: "input",
+      message: "Enter engineer's name: ",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "Enter engineer's ID: ",
+      name: "id",
+    },
+    {
+      type: "input",
+      message: "Enter engineer's email: ",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "Enter engineer's GitHub username: ",
+      name: "github",
+    },
+  ];
+  const intern = [
+    {
+      type: "input",
+      message: "Enter intern's name: ",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "Enter intern's ID: ",
+      name: "id",
+    },
+    {
+      type: "input",
+      message: "Enter intern's email",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "Enter intern's school: ",
+      name: "school",
+    },
+  ];
 
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log("Everything is good!")
-        }
-    });
+  
 }
 
-function writeHTML() {
-    inquirer.prompt().then((data) => {
-        const HTML = markdownHTML(data);
-        writeToFile('index.html', HTML)
-    })
-}
+
+
 
 writeHTML();
