4969493abc8ed6e439a4b6008b68d97dc9202a61
Hunter Wilson <huntertheguy123@gmail.com>
2023-03-09 16:40:31 -0600

    I added a style.css to help with the style of site and played around with the templates

--- src/HTML.js
+++ src/HTML.js
@@ -1,40 +1,88 @@
+const engineer = require("../lib/Engineer");
+const intern = require("../lib/Intern");
 
-const createTeam = team => {
-
-    //makes the manager card
-    const managerCard = manager => {
-        return `
+const createTeam = (team) => {
+  //makes the manager card
+  const managerCard = (manager) => {
+    return `
+    <div class="card">   
         <div class="card-header bg-primary text-white">
             <h2 class="card-title">${manager.getName()}</h2>
         </div>
         <div class="card-body">
             <ul class="list-group">
                 <li class="list-group-item">ID: ${manager.getId()}</li>
-                <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</li>
+                <li class="list-group-item">Email: ${manager.getEmail()}</li>
                 <li class="list-group-item">Office Number: ${manager.getOfficeNumber()}</li>
             </ul>
         </div>
-    
-        `
-    }
+    </div> 
+            `;
+  };
 
-//createTeam makes an empty array
-const html = [];
+  const engineerCard = (engineer) => {
+    return `
+    <div class="card">     
+        <div class="card-header bg-primary text-white">
+            <h2 class="card-title">${engineer.getName()}</h2>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${engineer.getId()}</li>
+                <li class="list-group-item">Email: ${engineer.getEmail()}</li>
+                <li class="list-group-item">Github username: ${engineer.getGithub()}</li>
+            </ul>
+        </div>
+    </div> 
+             `;
+  };
 
-//pushes to html array when it filters out those with role of manager
-//map is called on filtered manager array which generates html strings by the managerCard
-html.push(team
-    .filter(employee =>employee.getRole() === "Manager")
-    .map(manager => managerCard(manager))
-    );
+  const internCard = (intern) => {
+    return `
+    <div class="card">   
+        <div class="card-header bg-primary text-white">
+            <h2 class="card-title">${intern.getName()}</h2>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${intern.getId()}</li>
+                <li class="list-group-item">Email: ${intern.getEmail()}</li>
+                <li class="list-group-item">School: ${intern.getSchool()}</li>
+            </ul>
+        </div>
+    </div> 
+            `;
+  };
 
-    //html array is joined into string and returned to createTeam function.
-    return html.join("");
-}
+  //createTeam makes an empty array
+  const html = [];
+
+  //pushes to html array when it filters out those with role of manager
+  //map is called on filtered manager array which generates html strings by the managerCard
+  html.push(
+    team
+      .filter((employee) => employee.getRole() === "Manager")
+      .map((manager) => managerCard(manager))
+  );
+
+  html.push(
+    team
+      .filter((employee) => employee.getRole() === "Engineer")
+      .map((engineer) => engineerCard(engineer))
+  );
+
+  html.push(
+    team
+      .filter((employee) => employee.getRole() === "Intern")
+      .map((intern) => internCard(intern))
+  );
+  //html array is joined into string and returned to createTeam function.
+  return html.join("");
+};
 
 // exports html template as a string with team as argument
-module.exports = team => {
-    return `
+module.exports = (team) => {
+  return `
     <!DOCTYPE html>
     <html lang="en">
     <head>
@@ -42,6 +90,7 @@ module.exports = team => {
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="dist/style.css">
         <title>Team Creator</title>
     </head>
     <body>
@@ -54,11 +103,12 @@ module.exports = team => {
         </div>
         <div class ="container">
             <div class="row">
-                <div class ="row col-12 justify-content-center">
-                ${createTeam(team)}
+                <div class ="row col-12 d-flex justify-content-center">
+                        ${createTeam(team)}
+                </div>
             </div>
         </div>
     </body>
     </html>
-    `
-}
+    `;
+};
